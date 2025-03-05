6d3b789ab6c3092f70d60c3351253f5ea75c14bd
mayank sharma <mayank.rc.177@gmail.com>
2025-03-05 12:30:01 +0530

    keyclock tocken generated with frontend react+vite

--- src/keycloak.jsx
+++ src/keycloak.jsx
@@ -1,19 +1,13 @@
 import Keycloak from 'keycloak-js';
+import config from './config'; // Import the configuration file
 
 const keycloakConfig = {
-    url: 'http://localhost:8180', // Keycloak server URL
-    realm: 'myrealm', // Your realm name
-    clientId: 'myclient-new', // Your client ID
-    // url: process.env.REACT_APP_KEYCLOAK_URL,
-    // realm: process.env.REACT_APP_KEYCLOAK_REALM,
-    // clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID
+    url: config.keycloak.url, 
+    realm: config.keycloak.realm, 
+    clientId: config.keycloak.clientId, 
+   
 };
 
-
-
 console.log("in keycloak login");
-
 const keycloak = new Keycloak(keycloakConfig);
-// console.log("in keycloak login  "+keycloak.authenticated.authenticated);
-
 export default keycloak;
